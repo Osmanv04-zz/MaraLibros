25e5907253f9b542980f0549485b279a84eedf78
Osmanv04 <osmanv04@gmail.com>
2016-03-17 12:14:29 -0430

    Con carrusel

--- resources/views/index.php
+++ resources/views/index.php
@@ -10,10 +10,13 @@
     <!-- Custom Theme files -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+    <meta name="keywords" content="Cinema Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!--webfont-->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 <!-- header-section-starts -->
@@ -48,42 +51,44 @@
                 </div><!--/.nav-collapse -->
 
             </div>
-            <div class="more-reviews">
-                <ul id="flexiselDemo2">
-                    <li><img src="images/m1.jpg" alt=""/></li>
-                    <li><img src="images/m2.jpg" alt=""/></li>
-                    <li><img src="images/m3.jpg" alt=""/></li>
-                    <li><img src="images/m5.jpg" alt=""/></li>
-                </ul>
-                <script type="text/javascript">
-                    $(window).load(function() {
+            <div class="container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
 
-                        $("#flexiselDemo2").flexisel({
-                            visibleItems: 4,
-                            animationSpeed: 1000,
-                            autoPlay: true,
-                            autoPlaySpeed: 3000,
-                            pauseOnHover: false,
-                            enableResponsiveBreakpoints: true,
-                            responsiveBreakpoints: {
-                                portrait: {
-                                    changePoint:480,
-                                    visibleItems: 2
-                                },
-                                landscape: {
-                                    changePoint:640,
-                                    visibleItems: 3
-                                },
-                                tablet: {
-                                    changePoint:768,
-                                    visibleItems: 3
-                                }
-                            }
-                        });
-                    });
-                </script>
-                <script type="text/javascript" src="js/jquery.flexisel.js"></script>
-            </div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+     <img src="images/imgcarrusel.png" alt="Chania">
+
+    </div>
+
+    <div class="item">
+      <img src="images/imgcarrusel2.png" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="images/imgcarrusel3.png" alt="Flower">
+    </div>
+
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
             <!--
             <div class="review-slider">
                 <ul id="flexiselDemo1">
@@ -125,6 +130,46 @@
             </div>
             -->
         </div>
+
+        <div class="textopresentacion">
+            <p>hola como estan</p>
+        </div>
+        <div class="more-reviews">
+                <ul id="flexiselDemo2">
+                    <li><img src="images/m1.jpg" alt=""/></li>
+                    <li><img src="images/m2.jpg" alt=""/></li>
+                    <li><img src="images/m3.jpg" alt=""/></li>
+                    <li><img src="images/m5.jpg" alt=""/></li>
+                </ul>
+                <script type="text/javascript">
+                    $(window).load(function() {
+
+                        $("#flexiselDemo2").flexisel({
+                            visibleItems: 4,
+                            animationSpeed: 1000,
+                            autoPlay: true,
+                            autoPlaySpeed: 3000,
+                            pauseOnHover: false,
+                            enableResponsiveBreakpoints: true,
+                            responsiveBreakpoints: {
+                                portrait: {
+                                    changePoint:480,
+                                    visibleItems: 2
+                                },
+                                landscape: {
+                                    changePoint:640,
+                                    visibleItems: 3
+                                },
+                                tablet: {
+                                    changePoint:768,
+                                    visibleItems: 3
+                                }
+                            }
+                        });
+                    });
+                </script>
+                <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+            </div>
 
         <div class="news">
             <div class="col-md-6 news-left-grid">
